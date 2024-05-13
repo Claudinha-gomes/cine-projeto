ee4537f3fef82f4e84f7f601300225cd56881adf
cludinha-gomes <gomesdasilvaclaudia19@gmail.com>
2024-05-13 15:12:19 -0300

    inicio do projeto Spring

---
+++ src/test/java/br/com/alura/screenmatch/cine/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
